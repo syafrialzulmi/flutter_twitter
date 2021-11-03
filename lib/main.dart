94ac7d61113719af3d03acce6feaca6467805d5b
Syafrial Zulmi <syafrialzulmi@gmail.com>
2021-11-03 10:20:43 +0700

    ui flutter twitter

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 void main() {
   runApp(const MyApp());
@@ -7,109 +9,187 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    return const MaterialApp(
+      title: "Twitter",
+      debugShowCheckedModeBanner: false,
+      home: MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+  const MyHomePage({Key? key}) : super(key: key);
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  _MyHomePageState createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+        centerTitle: true,
+        elevation: 2,
+        backgroundColor: Colors.white,
+        leading: Container(
+          margin: EdgeInsets.all(10),
+          child: CircleAvatar(
+            backgroundImage: AssetImage('assets/images/CaptainJackSparrow.jpg'),
+          ),
+        ),
+        title: SvgPicture.asset(
+          'assets/icons/twitter.svg',
+          color: Color(0xFF1DA1F2),
+          height: 25,
+          width: 25,
+        ),
+        actions: [
+          Padding(
+            padding: const EdgeInsets.all(10),
+            child: SvgPicture.asset(
+              'assets/icons/switchTimeline.svg',
+              color: Color(0xFF1DA1F2),
+              height: 25,
+              width: 25,
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+          )
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: Icon(
+          Icons.add,
+        ),
+        backgroundColor: Color(0xff1D9BF0),
+      ),
+      body: ListView(
+        children: <Widget>[
+          _buildListOdTweets("assets/images/user1.jpg", "Tiffany Nisa",
+              "@tiffanynisa", "10 m", "sastro", "100", "5", "30", "150"),
+          _buildListOdTweets("assets/images/user2.jpg", "Syafrial Zulmi",
+              "@syafrialzulmi", "1 h", "meong", "20", "51", "90", "50"),
+          _buildListOdTweets("assets/images/user3.jpg", "Papah Muda",
+              "@papahmuda", "yesterday", "Johny", "10", "3", "3", "250")
+        ],
+      ),
+      bottomNavigationBar: BottomAppBar(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: <Widget>[
+            IconButton(
+              onPressed: () {},
+              icon: Icon(FontAwesomeIcons.home),
+            ),
+            IconButton(
+              onPressed: () {},
+              icon: Icon(FontAwesomeIcons.search),
+            ),
+            IconButton(
+              onPressed: () {},
+              icon: Icon(FontAwesomeIcons.bell),
+            ),
+            IconButton(
+              onPressed: () {},
+              icon: Icon(FontAwesomeIcons.envelope),
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+
+  _tweetAvatar(imageAccount) {
+    return Container(
+      margin: const EdgeInsets.all(10.0),
+      child: CircleAvatar(
+        backgroundImage: AssetImage(imageAccount),
+      ),
+    );
+  }
+
+  _tweetIconButton(iconsTweet, teksNumber) {
+    return Row(
+      children: <Widget>[
+        Icon(
+          iconsTweet,
+          color: Colors.black45,
+          size: 18.0,
+        ),
+        Container(
+          margin: EdgeInsets.all(6.0),
+          child: Text(
+            teksNumber,
+            style: TextStyle(color: Colors.black45, fontSize: 14.0),
+          ),
+        )
+      ],
+    );
+  }
+
+  _tweetBody(nameUser, accountUser, tweetTime, tweetText, commentNumber,
+      retweetNumber, heartNumber, shareNumber) {
+    return Expanded(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Row(
+            children: <Widget>[
+              Container(
+                margin: EdgeInsets.only(right: 10.0),
+                child: Text(
+                  nameUser,
+                  style: TextStyle(
+                      color: Colors.black, fontWeight: FontWeight.bold),
+                ),
+              ),
+              Text(
+                accountUser + " • " + tweetTime,
+                style: TextStyle(color: Colors.grey),
+              ),
+              Spacer(),
+              IconButton(
+                onPressed: () {},
+                icon: Icon(
+                  FontAwesomeIcons.angleDown,
+                  size: 14.0,
+                  color: Colors.grey,
+                ),
+              ),
+            ],
+          ),
+          Text(
+            tweetText,
+            overflow: TextOverflow.clip,
+          ),
+          Container(
+            margin: EdgeInsets.only(top: 10, right: 20),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                _tweetIconButton(FontAwesomeIcons.comment, commentNumber),
+                _tweetIconButton(FontAwesomeIcons.retweet, retweetNumber),
+                _tweetIconButton(FontAwesomeIcons.heart, heartNumber),
+                _tweetIconButton(FontAwesomeIcons.share, shareNumber),
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
+  _buildListOdTweets(imageAccount, nameUser, accountUser, tweetTime, tweetText,
+      commentNumber, retweetNumber, heartNumber, shareNumber) {
+    return Row(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        _tweetAvatar(imageAccount),
+        _tweetBody(nameUser, accountUser, tweetTime, tweetText, commentNumber,
+            retweetNumber, heartNumber, shareNumber)
+      ],
     );
   }
 }
